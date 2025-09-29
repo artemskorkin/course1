8e53955dc7e90f1e2bec37b47264233bec0bc53c
artemskorkin <artemskorkin@yandex.ru>
2025-09-29 16:42:16 +0700

    Добавили img

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var myColorScheme = ColorScheme.fromSeed(seedColor: Colors.green);
 
-
     return MaterialApp(
       title: "Flutter App",
       theme: ThemeData(
         colorScheme: myColorScheme,
-        appBarTheme: AppBarTheme(
-          backgroundColor: myColorScheme.inversePrimary,
-        ),
+        appBarTheme: AppBarTheme(backgroundColor: myColorScheme.inversePrimary),
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Flutter App")
-        ),
+        appBar: AppBar(title: const Text("Flutter App")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("Изучаем Flutter"),
-              ElevatedButton(onPressed: () {}, child: Text("Ok")),
+              Image.asset("assets/images/pizza.webp", width: 250),
+              const Text("Вкусная пицца"),
+              ElevatedButton(
+                onPressed: () {
+                  print("Вы купили пиццу! Спасибо за заказ!");
+                },
+                child: const Text("Купить"),
+              ),
               FlutterLogo(size: 50.0),
             ],
           ),
