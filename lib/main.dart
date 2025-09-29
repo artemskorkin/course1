9c895d7d103acb9a3f6ebd9e15eda40957e46bec
artemskorkin <artemskorkin@yandex.ru>
2025-09-29 17:15:25 +0700

    Отдельный файл для ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'app_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,29 +10,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var myColorScheme = ColorScheme.fromSeed(seedColor: Colors.green);
-
+ 
     return MaterialApp(
       title: "Flutter App",
-      theme: ThemeData(
-        colorScheme: myColorScheme,
-        appBarTheme: AppBarTheme(backgroundColor: myColorScheme.inversePrimary),
-      ),
+      theme: AppTheme.lightTheme(),
       home: Scaffold(
         appBar: AppBar(title: const Text("Flutter App")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/images/pizza.webp", width: 250),
-              const Text("Вкусная пицца"),
+              Image.asset("assets/images/pizza.png", width: 250),
+              Text("Вкусная пицца", style: Theme.of(context).textTheme.headlineMedium
+              ),
               ElevatedButton(
                 onPressed: () {
                   print("Вы купили пиццу! Спасибо за заказ!");
                 },
-                child: const Text("Купить"),
+                child: Text("Купить", 
+                style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                  fontWeight: FontWeight.bold,color: Colors.white, fontSize: 20,
+                )),
               ),
-              FlutterLogo(size: 50.0),
             ],
           ),
         ),
